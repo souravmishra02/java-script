b27bab2b94206a23a217e7eda717077ef04b4db3
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-29 06:33:03 +0000

    config

---
+++ 01_basics/variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "sourav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountEmail = "sarthak@gail.com"
+accountPassword = "dfrw233"
+accountCity = "bengaluru"
+//  always use const and let because for the scope related isssue like  block scope and fucntional scope, preferred not to use var
+console.table([accountEmail, accountPassword, accountCity])
