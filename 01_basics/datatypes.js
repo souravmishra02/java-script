a19ac8fa19588797f4f9dd9ba32dbbf47a18a52b
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-29 07:01:57 +0000

    commit

--- 01_basics/datatypes.js
+++ 01_basics/datatypes.js
@@ -1,6 +1,24 @@
 "use strict" ;// for the newer version of the js
 // alert(3+3) // we are using nodejs not browser
 
-console.log(3+3)
+console.log(3+3)   //code readability is very inportant 
 console.log("sourav")
+
+let name = "sourav" //string datatype
+let age = 18 //number datatypes
+let isLoggedIn =true/false // boolean datatype
+
+
+//Premitive Datatypes
+// Number Datatype => 2 to power 53
+// string Datatype => "" use in double ""'s
+// bollean Datatype => yes/No, true/false
+//  Null => standalone Value   representation of an empty array for ex: let temprature = Null value is coming to be 0 and we need that to be NULL to be printed objects 
+// undefined => value doesnot define for ex: let state;   value is not assign yet   undefined type
+// symbol it is used by the figma tools and it is basically use for uniqness
+
+// Objects
+//Null
+
+console.log(typeof null);
 
