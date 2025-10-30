fcf070da6e32e35951f969a35a42478e7b94228a
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-30 07:41:30 +0000

    changes

--- 01_basics/variable.js
+++ 01_basics/variable.js
@@ -1,11 +1,31 @@
-const accountId = 144553
-let accountEmail = "sourav@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
+// const accountId = 144553
+// let accountEmail = "sourav@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
 
-accountEmail = "sarthak@gail.com"
-accountPassword = "dfrw233"
-accountCity = "bengaluru"
-let accountState
-//  always use const and let because for the scope related isssue like  block scope and fucntional scope, preferred not to use var
-console.table([accountEmail, accountPassword, accountCity, accountState])
+// accountEmail = "sarthak@gail.com"
+// accountPassword = "dfrw233"
+// accountCity = "bengaluru"
+// let accountState
+// //  always use const and let because for the scope related isssue like  block scope and fucntional scope, preferred not to use var
+// console.table([accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+let score = false
+let sys = Number(score)
+// console.log(typeof sys);
+// console.log(sys);
+//  if we convert a number then it easily convert
+//  if we try to convert 33abc it will not get convert it will give NAN(not a number)
+
+let boo = "sourav"
+let goo = Boolean(boo)
+console.log(typeof goo);
+console.log(goo);
+
+// 1 -> true
+//  0-> false
+//  if empty string -> false
+//  we add something in the empty string -> true
