cf400faec8c13dabe81643bdf6f703c199f93b75
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-30 14:48:35 +0000

    change

---
+++ 01_basics/comparison.js
@@ -0,0 +1,9 @@
+// console.log(4>2);
+// console.log("2">1);
+// console.log(null > 2);
+// console.log(null >=0);
+
+//  Strict check === check the value and its datatype both
+
+
+
---
+++ 01_basics/operations.js
@@ -0,0 +1,27 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "sourav"
+let str2 = " mishra"
+let str3 = str1+str2
+// console.log(str3);
+
+
+// console.log(2+4+ "3"); // As in JavaScript the operations (add) take place before the string only
+//  In JavaScript if String first then every one will be treated like a String if string is in last all the operation will take place first and add the string in the last
+
+
+let score = 100
+++score
+console.log(score);
+
+
