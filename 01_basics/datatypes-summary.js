69a18b092bf93a49a1ec290e7e0589445cbcf2c9
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-30 15:48:50 +0000

    commit changes

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -8,15 +8,29 @@
 
 //  Function actually return function and we call them object function 
 
-const isLoggedIn = function()
-{
-    if(2>0){
-        console.log("True")
-    }
-    else{
-        console.log("False");
+// const isLoggedIn = function()
+// {
+//     if(2>0){
+//         console.log("True")
+//     }
+//     else{
+//         console.log("False");
         
-    }
-}
-isLoggedIn()
+//     }
+// }
+// isLoggedIn()
 
+
+// There are two types of memory 
+// Heap memory and stack memory 
+//  Stack memory is used for primitive datatypes as whereever stack memory is used we will get the copy of the varibale which we created in this when ever we want to do any changes we will do that in the copy first and then it reflect 
+//  Heap memory is used for the non primitive datatypes as whenever the heap memory is used we will get the reference of the varibale which we create and then we do the changes in the original variable
+
+
+// Stack(primitive datatype) - string nmmber, bollean, null , undefined, symbol , bigInt
+//  heap(refrence) - array , object , function 
+
+
+let id = "sourav"
+let another = id
+console.log(id)
