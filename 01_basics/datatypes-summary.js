576dcd21fee4a9ff91ee09e13dca00bc279a020c
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-30 15:11:41 +0000

    chnage

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// There are two types of Data types
+// premitive Data Types
+// Non premitive/ Reference 
+//  In premitive there are 7 types of data types: "String", Number, Boolean , Null , Undefined , symbol, BigInt
+// Reference type (non premitive) arrays, objects , functions
+
+//  JavaScript is dynamically types language or statically type language. as we dont  : dynamically typed
+
+//  Function actually return function and we call them object function 
+
+const isLoggedIn = function()
+{
+    if(2>0){
+        console.log("True")
+    }
+    else{
+        console.log("False");
+        
+    }
+}
+isLoggedIn()
+
