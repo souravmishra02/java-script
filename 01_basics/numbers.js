9684cb73221ca0f1531839992cd6b04bdae3b9f3
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-31 09:09:21 +0000

    numbers commit

---
+++ 01_basics/numbers.js
@@ -0,0 +1,8 @@
+// Numbers
+const score = new Number(123.454)
+
+console.log(score.toString().length); // convert to string and then calculate the length
+
+console.log(score.toFixed(1)); // fixed method
+
+console.log(score.toPrecision(3)); //to precisiom method
