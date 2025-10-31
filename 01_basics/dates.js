3a1182bdb6e1bec917818321a17e4b9ff6bb46c5
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-31 14:39:19 +0000

    changes in dates

---
+++ 01_basics/dates.js
@@ -0,0 +1,8 @@
+let datem = new Date()
+// console.log(datem.toString());
+// console.log(datem.toLocaleString());
+// console.log(datem.toDateString());
+
+let myCreated = new Date(2002,11,17)
+console.log(myCreated.toDateString());
+
