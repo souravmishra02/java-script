d14322dc926877372feb8e27943a6b62f10995d7
sourav mishra <97212993+souravmishra02@users.noreply.github.com>
2025-10-31 08:51:09 +0000

    string change

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -3,7 +3,11 @@ const repoCount = 50
 // console.log(` hey my name is ${name.toUpperCase()} and the repo count is ${repoCount}`)
 
 const gameName = new String('hello')
-console.log(gameName.charAt(3));
-console.log(gameName.toUpperCase());
-console.log(gameName.length);
+// console.log(gameName.charAt(3));
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
 
+
+const url = "https://sourav%20mishra"
+
+console.log(url.replace('%20', '-'))
